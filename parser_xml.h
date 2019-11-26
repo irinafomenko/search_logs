d861f312dfd7b0822a64940192ce945861c350e6
Fomenko Irina <irina.fomenko@mera.com>
2019-11-26 11:55:56 +0300

    Fixed pars_xml() function

--- parser_xml.h
+++ parser_xml.h
@@ -18,6 +18,6 @@ void print_node(rapidxml::xml_node<> * node);
 void print_result(rapidxml::xml_node<> * node);
 void search_tag(rapidxml::xml_node<> * root_node, std::string name_tag = "", std::string value_tag = "");
 void parsing_xml();
-int pars_xml(std::string file);
+std::string pars_xml(std::string file);
 
 #endif //PROJECT_LOGS_PARSER_XML_H
