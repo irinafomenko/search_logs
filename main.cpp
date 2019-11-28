7d0ecbc50782cc38499cc268f08036e83826cc83
Fomenko Irina <irina.fomenko@mera.com>
2019-11-28 18:19:46 +0300

    Fixed main

--- main.cpp
+++ main.cpp
@@ -1,131 +1,57 @@
 #include <iostream>
 #include <string>
-//#include "rapidxml-1.13/rapidxml.hpp"
-//#include "rapidxml-1.13/rapidxml_print.hpp"
-//#include "rapidxml_ext.h"
-#include "enum_tags.h"
+#include "pars_txt.h"
 #include "class_XML.h"
 #include "parser_xml.h"
 #include "search_calls_in_slots.h"
+
 
 using namespace rapidxml;
 using namespace std;
 
 XML create_xml;
-
-void read_log_file(string file)
-{
-    bool flag_child;//флаг для создания дерева
-    string log_str, sub_str;
-    string value_sub_str = "";// = "SIP/2.0";
-    ifstream log_file(file,ios::in);
-
-    if(log_file.is_open())
-    {
-        //string line = "";
-        while(!log_file.eof())
-        {
-            getline(log_file, log_str);
-            int p1, p2, p4;//позиции вхождений подстрок
-            int sub_start = INVITE;
-            int sub_end = SIP;
-            while(sub_start <= sub_end) //поиск тега по возможным значениям
-            {
-                sub_str = enum_to_str(sub_start);
-                p1 = log_str.find("<" + sub_str);
-                p4 = log_str.find("<^" + sub_str);
-                if(sub_start == SIP)
-                {
-                    sub_str = "SIP.2.0";
-                    if(p4 >= 0)
-                    {
-                        value_sub_str = log_str.substr(p4 + sub_str.length() + 3);
-                        value_sub_str[value_sub_str.length() - 1] = 0;
-                    } //без пробела
-                    else if(p1 >= 0)
-                    {
-                        value_sub_str = log_str.substr(p1 + sub_str.length() + 2);
-                        value_sub_str[value_sub_str.length() - 1] = 0;
-                    }
-                    //cout << value_sub_str << endl;
-                }
-                if(p1 >= 0 || p4 >= 0) {break;}
-                sub_start++;
-            }
-            p2 = log_str.find(">|");
-            if(p1 >= 0 || p4 >= 0)
-            {
-                /*---------------------------------------------*/
-                flag_child = false;
-                create_xml.add_child(flag_child, sub_str, value_sub_str);
-                /*---------------------------------------------*/
-                //дата
-                p1 = log_str.find('@');
-                flag_child = true;
-                create_xml.add_child(flag_child, "Date", log_str.substr(p1 + 1, 10));
-                /*---------------------------------------------*/
-                //время
-                p1 = log_str.find(' ');
-                create_xml.add_child(flag_child, "Time", log_str.substr(p1 + 1, 8));
-                /*---------------------------------------------*/
-                while(p2 < 0)
-                {
-                    getline(log_file, log_str);
-                    p2 = log_str.find(">|");
-                    if(p2 == 0) {break;}
-                    int p3 = log_str.find(": ");//позиция вхождения подстроки
-                    string value_log;
-                    if(p3 < 0)
-                    {
-                        p3 = log_str.find('=');
-                        value_log = log_str.substr(p3 + 1);//значение строки без знака =
-                        value_log[value_log.length() - 1] = 0;
-                        if(p3 < 0) { continue;}
-                    }
-                    else
-                    {
-                        value_log = log_str.substr(p3 + 2);//значение строки без знака : и пробела
-                        value_log[value_log.length() - 1] = 0;
-                    }
-                    //cout << log_str.substr(0, p3) << endl;
-                    /*---------------------------------------------*/
-                    //flag_child = true;
-                    create_xml.add_child(flag_child, log_str.substr(0, p3).c_str(), value_log);
-                    /*---------------------------------------------*/
-                }
-
-            }
-        }
-        log_file.close();
-    }
-    else
-    {
-        cout << "Log_file doesn't open!" << endl;
-    }
-}
+//ofstream res_file("SessionManager.txt",ios::out);//попытка без xml
+ofstream result_logs_file("results_search_logs.txt", ios::out);
+string number_sid;//номер слота
 
 int main()
 {
     string new_xml_file = "SessionManager.xml";
     string time;
-    /*---------------------------------------------*/
-    create_xml.open_file(new_xml_file);
-    create_xml.add_root();
-    /*---------------------------------------------*/
+    string name_tag, value_tag;
+    cout << "Enter name tag: ";
+    getline(cin, name_tag);
+    cout << "Enter value tag: ";
+    getline(cin, value_tag);
+
     for(int i=0; i<5; i++)
     {
         string file;
         if(i == 0) {file = "C:/Users/ifomenko/Desktop/prjct_logs/opt/Avaya/ExperiencePortal/MPP/logs/process/SessMgr/SessionManager.log";}
         else {file = "C:/Users/ifomenko/Desktop/prjct_logs/opt/Avaya/ExperiencePortal/MPP/logs/process/SessMgr/SessionManager.log." + to_string(i);}
         cout << file << endl;
-        read_log_file(file);
+        //read_log_file(file);
+        time = pars_txt(file, name_tag, value_tag);
+        if(!number_sid.empty()) {break;}
     }
+    cout << "Start time and end time: ";
+    cout << time << endl;
+    //cout << number_sid << endl;
+    result_logs_file.close();
     /*---------------------------------------------*/
-    create_xml.close_file();
+    //номер слота
+    //number_sid = number_sid.substr(number_sid.find('-') + 15);
+    char *s = new char[number_sid.size() + 1];
+    strcpy(s, number_sid.c_str());
+    char *p = strtok(s,"-");
+    while (p != NULL)
+    {
+        number_sid = p;
+        p = strtok(NULL, "-");
+    }
+    //cout << number_sid << endl;
     /*---------------------------------------------*/
-    time = pars_xml(new_xml_file);
-    /*---------------------------------------------*/
-    search_calls(time);
+    search_calls(time, number_sid);
     /*---------------------------------------------*/
     return 0;
 }
