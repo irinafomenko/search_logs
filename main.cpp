4b0ce0239b3eee17bd6b12e1ce4fd77138a20a2b
Fomenko Irina <irina.fomenko@mera.com>
2019-12-02 14:47:22 +0300

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <string>
 #include "pars_txt.h"
-#include "class_XML.h"
-#include "parser_xml.h"
-#include "search_calls_in_slots.h"
+#include "search_calls.h"
 
-
-using namespace rapidxml;
 using namespace std;
 
-XML create_xml;
-//ofstream res_file("SessionManager.txt",ios::out);//попытка без xml
-ofstream result_logs_file("results_search_logs.txt", ios::out);
-string number_sid;//номер слота
+ofstream result_logs_file("SM_SIP_result.txt", ios::out);
+string number_slot;//номер слота
 
 int main()
 {
@@ -32,26 +26,32 @@ int main()
         cout << file << endl;
         //read_log_file(file);
         time = pars_txt(file, name_tag, value_tag);
-        if(!number_sid.empty()) {break;}
+        //if(!number_slot.empty()) {break;}
     }
     cout << "Start time and end time: ";
     cout << time << endl;
     //cout << number_sid << endl;
     result_logs_file.close();
     /*---------------------------------------------*/
-    //номер слота
-    //number_sid = number_sid.substr(number_sid.find('-') + 15);
-    char *s = new char[number_sid.size() + 1];
-    strcpy(s, number_sid.c_str());
+    //поиск номера слота
+    //в строке содержится имя машины-id-номер слота
+    //15 позиция номера слота после первого знака "-"
+    number_slot = number_slot.substr(number_slot.find('-') + 15);
+    /*
+    char *s = new char[number_slot.size() + 1];
+    strcpy(s, number_slot.c_str());
     char *p = strtok(s,"-");
     while (p != NULL)
     {
-        number_sid = p;
+        number_slot = p;
         p = strtok(NULL, "-");
-    }
-    //cout << number_sid << endl;
+    }*/
+#ifdef DEBUG
+    cout << number_slot << endl;
+#endif
     /*---------------------------------------------*/
-    search_calls(time, number_sid);
+    //поиск звонков в файлах
+    search_calls(time, number_slot);
     /*---------------------------------------------*/
     return 0;
 }
