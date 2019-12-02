892ffc9a6c1f1e34c24344bc95bafe212dd05c1f
Fomenko Irina <irina.fomenko@mera.com>
2019-12-02 15:15:17 +0300

    Added functions

--- search_calls.h
+++ search_calls.h
@@ -2,8 +2,8 @@
 // Created by ifomenko on 26.11.2019.
 //
 
-#ifndef PROJECT_LOGS_SEARCH_CALLS_IN_SLOTS_H
-#define PROJECT_LOGS_SEARCH_CALLS_IN_SLOTS_H
+#ifndef PROJECT_LOGS_SEARCH_CALLS_H
+#define PROJECT_LOGS_SEARCH_CALLS_H
 
 #include <iostream>
 #include <fstream>
@@ -11,5 +11,9 @@
 
 int search_calls(std::string time, std::string number_slot);
 void write_file(std::string slot);
+void open_slots(std::string number_slot);
+void open_cxi_slots(std::string number_slot);
+void open_vb_slots(std::string number_slot);
+void open_end_point_mgr(std::string number_slot);
 
-#endif //PROJECT_LOGS_SEARCH_CALLS_IN_SLOTS_H
+#endif //PROJECT_LOGS_SEARCH_CALLS_H
